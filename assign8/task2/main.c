60d7a308a553dca06b2c16ebf4fcb8bc92360f6c
chen <kaiyinchen.kc@gmail.com>
2021-04-26 14:03:39 +0000

    update

--- assign8/task2/main.c
+++ assign8/task2/main.c
@@ -1,4 +1,4 @@
 int main() {
-    int a=13,b=5, c;
-    c=a+b;
+  int a = 13, b = 5;
+  int c = a*(b - a%b);
 }
--- assign8/task4/main.c
+++ assign8/task4/main.c
@@ -1,13 +1,21 @@
 char password[8] = "secret";
 int main() {
-    char input[8];
-    printf("please enter your password\n");
-    scanf("%s", input);
-
-    if(strcmp(input, password)==0) {
-        return 0;
-    } else {
-        return -1;
+  char input[8];
+  int i;
+   
+  printf("please enter your password\n");
+  scanf("%s", input);
+    
+  for(i = 0; i < 8; i++){
+    if(65 <= input[i] && 90 >= input[i]){
+      input[i] = 32 + input[i];
     }
+  }
+
+  if(strcmp(input, password)==0){
+    return 0;
+  }else{
+    return -1;
+  }
 
 }
